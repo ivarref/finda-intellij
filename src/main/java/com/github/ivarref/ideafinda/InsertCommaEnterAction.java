18e14b6af1741adf236da60618e8134b45b28a41
ire <ivar.refsdal@sikt.no>
2025-06-29 19:53:59 +0200

    Begin devil comma

---
+++ src/main/java/com/github/ivarref/ideafinda/InsertCommaEnterAction.java
@@ -0,0 +1,21 @@
+package com.github.ivarref.ideafinda;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorModificationUtilEx;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
+
+// https://intellij-support.jetbrains.com/hc/en-us/community/posts/206804965-problem-inserting-text-from-menu-action
+public class InsertCommaEnterAction extends EditorAction {
+    public InsertCommaEnterAction() {
+        super(new Handler());
+    }
+
+    private static class Handler extends EditorWriteActionHandler {
+        public final void executeWriteAction(Editor editor, Caret caret, DataContext dataContext) {
+            EditorModificationUtilEx.insertStringAtCaret(editor, ",\n");
+        }
+    }
+}
